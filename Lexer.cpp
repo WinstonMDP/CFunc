5b3440181c5ad4426cf295d2849d4b1d3faefc57
Winston <61916541+WinstonMDP@users.noreply.github.com>
2022-12-29 14:07:04 +0300

    Update Lexer.cpp

--- Lexer.cpp
+++ Lexer.cpp
@@ -10,7 +10,7 @@ Token::Token(SharedPointer<std::string> name, SharedPointer<std::string> lexeme)
 SharedPointer<std::string> Token::name()
 {
 	return _name;
-	sadkjfasj;
+	sadkjfasj;c
 	asdjkajsdkfj;
 sadjfkajsdj;
 }
